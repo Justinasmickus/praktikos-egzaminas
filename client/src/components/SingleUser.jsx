8dcc8b27576de2b172b2dc589be2665a3e4f5e49
Justinasmickus <mickusjustinas1@gmail.com>
2021-07-28 10:54:58 +0300

    single user and user list class components created

---
+++ client/src/components/SingleUser.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SingleUser extends Component {
+    state = {  }
+    render() { 
+        return ( <div className="single-user">
+            <h4>this is a single user</h4>
+        </div> );
+    }
+}
+ 
+export default SingleUser;
---
+++ client/src/components/UserList.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class UserList extends Component {
+    state = {  }
+    render() { 
+        return ( <div className="list-container">
+            <h3>User list</h3>
+        </div> );
+    }
+}
+ 
+export default UserList;
